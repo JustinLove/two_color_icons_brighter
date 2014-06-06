639200399fe1b6e7595008cb6012cc0ec3efafa9
Justin Love <git@JustinLove.name>
2014-06-06 09:58:41 -0500

    secondary outline, white when selected.

---
+++ shaders/particle_icon.fs
@@ -0,0 +1,35 @@
+#version 150
+
+// textured_unlit_bgra_colorized.fs
+
+#ifdef GL_ES
+precision mediump float;
+#endif
+
+uniform sampler2D Texture;
+
+in vec2 v_TexCoord;
+in vec4 v_ColorPrimary;
+in vec4 v_ColorSecondary;
+in float v_SelectedState;
+
+out vec4 out_FragColor;
+
+void main() 
+{
+    vec4 texel = texture(Texture, v_TexCoord).bgra;
+    if (v_ColorPrimary.w == 0.0)
+        out_FragColor = texel;
+    else
+    {
+        // horrible, horrible hack to work aground webkit png handling
+        texel.rgb = clamp((texel.rgb - 0.27) / 0.7, 0.0, 1.0);
+
+        vec3 outline = v_SelectedState > 0.0 ? vec3(1.0, 1.0, 1.0) : v_ColorSecondary.rgb;
+        vec3 body = v_ColorPrimary.rgb;
+        vec3 color = texel.r * mix(outline, body, pow(texel.g, 1.0 / 2.2));
+
+        float alpha = texel.a;
+        out_FragColor = vec4(color, alpha * v_ColorPrimary.a);
+    }
+}
